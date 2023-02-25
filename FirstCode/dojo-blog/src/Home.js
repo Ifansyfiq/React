76b05f52f4b583f1f3c287def709c3d097f9b68b
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-25 23:15:05 +0800

    update dojo-blog

--- FirstCode/dojo-blog/src/Home.js
+++ FirstCode/dojo-blog/src/Home.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
+    
+    //const name = 'hayai';
+    const [name, setName] = useState('hayai');
+    const [age, setAge] = useState(23);
+    
+    const handleClick = () => {
+        setName('saki');
+        setAge(39);
+    }
+    const handleClick2 = (name) => {
+        console.log('hello ' +name);
+    }
     return (  
         <nav className="home">
             <h1>HomePage</h1>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClick2('hayai')}>Click Meeeeeeeee</button>
         </nav>
     );
 }
  
-export default Home;
+export default Home; 
--- FirstCode/dojo-blog/src/Navbar.js
+++ FirstCode/dojo-blog/src/Navbar.js
@@ -3,7 +3,11 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Dojo Blog</h1>
             <a href="/">home</a>
-            <a href="/create">New Blog</a>
+            <a href="/create" style={{
+                color: "white",
+                backgroundColor: '#f1356d',
+                borderRadius: '8px'
+            }}>New Blog</a>
         </nav>  
      );
 }
