dc91a348a419274620ddef1ff2d0d07e969bdb23
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-26 12:34:17 +0800

    Update Home.js

--- FirstCode/dojo-blog/src/Home.js
+++ FirstCode/dojo-blog/src/Home.js
@@ -5,7 +5,13 @@ const Home = () => {
     //const name = 'hayai';
     const [name, setName] = useState('hayai');
     const [age, setAge] = useState(23);
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+
+
     const handleClick = () => {
         setName('saki');
         setAge(39);
@@ -19,6 +25,13 @@ const Home = () => {
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
             <button onClick={() => handleClick2('hayai')}>Click Meeeeeeeee</button>
+
+            {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
         </nav>
     );
 }
