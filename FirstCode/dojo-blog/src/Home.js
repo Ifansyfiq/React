ce253636f7925dfc28a3357271f04fae21224600
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-26 12:47:28 +0800

    props

--- FirstCode/dojo-blog/src/Home.js
+++ FirstCode/dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     
@@ -26,12 +27,7 @@ const Home = () => {
             <button onClick={handleClick}>Click Me</button>
             <button onClick={() => handleClick2('hayai')}>Click Meeeeeeeee</button>
 
-            {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
+            <BlogList blogs={blogs} title="All Blogs"/>
         </nav>
     );
 }
