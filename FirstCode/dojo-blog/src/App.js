c7d5975099812fdde1623f4e61badec9d062b03e
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-27 23:44:21 +0800

    React router

--- FirstCode/dojo-blog/src/App.js
+++ FirstCode/dojo-blog/src/App.js
@@ -1,18 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //declare router
 
 function App() {
   const desc = "Learning"
   return (
+    <Router> 
     <div className="App">
       <p>This is the {desc} session</p>
       <Navbar/> 
      <div className="content">
-      <Home />
-      
+     <Routes>
+          <Route path="/">
+              <Home />
+          </Route>
+      </Routes>
      </div>
     </div>
+    </Router>
   );
 }
 
