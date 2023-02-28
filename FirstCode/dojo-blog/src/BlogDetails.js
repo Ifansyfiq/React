d3cb7f4f3d0a690d6d656e8dc57c14c85e1d6f0b
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-28 13:43:58 +0800

    Update BlogDetails.js

--- FirstCode/dojo-blog/src/BlogDetails.js
+++ FirstCode/dojo-blog/src/BlogDetails.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
     const {id} = useParams()
-
+ 
     return (  
         <div className="blog-BlogDetails">
             <h2>Blog details - {id}</h2>
