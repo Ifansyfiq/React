d26e3965ad9819ae43b87b604587f961b61abc6b
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-28 10:59:37 +0800

    specify routing

--- FirstCode/dojo-blog/src/App.js
+++ FirstCode/dojo-blog/src/App.js
@@ -1,23 +1,24 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //declare router (Switch (V5) change with Routes (v6))
+import { BrowserRouter as  Routing ,Route, Routes } from 'react-router-dom'; //declare router (Switch (V5) change with Routes (v6))
+import Create from './Create';
+
 
 function App() {
   const desc = "Learning"
   return (
-    <Router> 
+    <Routing>
     <div className="App">
       <p>This is the {desc} session</p>
       <Navbar/> 
      <div className="content">
      <Routes>
-          <Route path="/">
-              <Home />
-          </Route>
-      </Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/create" element={<Create/>}/>
+     </Routes>
      </div>
     </div>
-    </Router>
+    </Routing>
   );
 }
 
