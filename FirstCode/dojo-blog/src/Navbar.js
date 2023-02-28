af5cc5f600671593e8d6997ec49ddd564e4cb9ec
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-02-28 11:01:35 +0800

    Update Navbar.js

--- FirstCode/dojo-blog/src/Navbar.js
+++ FirstCode/dojo-blog/src/Navbar.js
@@ -2,12 +2,14 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Dojo Blog</h1>
+            <div className="links">
             <a href="/">home</a>
             <a href="/create" style={{
                 color: "white",
                 backgroundColor: '#f1356d',
                 borderRadius: '8px'
             }}>New Blog</a>
+            </div>
         </nav>  
      );
 }
