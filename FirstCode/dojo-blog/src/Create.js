c871a89da65acbab9bdc73078bd87868ac764fbc
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-01 13:01:06 +0800

    Controlled Inputs (forms)

--- FirstCode/dojo-blog/src/Create.js
+++ FirstCode/dojo-blog/src/Create.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [Title, setTitle] = useState('');
+    const [Body, setBody] = useState('');
+    const [Author, setAuthor] = useState('');
+
     return ( 
+
         <div className="create">
             <h1>Add New blog</h1>
+            <form>
+                <label>Blog Title:</label>
+                <input type={Text} required value={Title} onChange = {(e) => setTitle(e.target.value)} ></input>
+                <label>Blog Body:</label>
+                <textarea required value={Body} onChange = {(e) => setBody(e.target.value)}></textarea>
+                <label>Blog Author:</label>
+                <select value={Author} onChange = {(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }   
