a8f9e2f2f3d92f68c1dc95f3b4d4dfb097f934b5
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-01 12:36:16 +0800

    reusing custom hook

--- FirstCode/dojo-blog/src/BlogDetails.js
+++ FirstCode/dojo-blog/src/BlogDetails.js
@@ -1,15 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams()
- 
+    const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id); //utk url letak '/' dkt hujung utk specify id dkt belakang
+
     return (  
         <div className="blog-BlogDetails">
-            <h2>Blog details - {id}</h2>
+            {error && <div>{error}</div>} 
+            {isPending && <div>Please Wait........</div>}
+            {blog && (
+                <article>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
-
-
 }
  
 export default BlogDetails
